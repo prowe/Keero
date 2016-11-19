8be23fe3d5cb1d7602c40d675ab1247589969e4d
Paul <paul.w.rowe@gmail.com>
2016-11-18 18:55:11 -0600

    adding financial ledger

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function getAbilityModifier(statName) {
 
 function populateModifierElements() {
     [].slice.call(document.querySelectorAll('[data-populate-modifier]'))
-        .forEach(function(e) {
+        .forEach(function (e) {
             var statName = e.attributes.getNamedItem('data-populate-modifier').value;
             e.textContent = getAbilityModifier(statName);
         });
@@ -25,7 +25,7 @@ function populateModifierElements() {
 
 function populateEvalForContent() {
     [].slice.call(document.querySelectorAll('[data-eval-for-content]'))
-        .forEach(function(e) {
+        .forEach(function (e) {
             var exp = e.attributes.getNamedItem('data-eval-for-content').value;
             e.textContent = eval(exp);
         });
@@ -33,26 +33,63 @@ function populateEvalForContent() {
 
 function registerRollExpressionHandlers() {
     [].slice.call(document.querySelectorAll('.roll-expression'))
-        .forEach(function(e) {
+        .forEach(function (e) {
             var exp = e.textContent;
             exp = exp.replace(/\+\s*0/g, ''); //fix adding 0
             //try {
-                var de = new DiceExpression(exp);
-                e.onclick = function (e) {
-                    e.preventDefault();
-                    var roll = de.roll();
-                    alert('result: ' + roll.roll + " = " + roll.diceRaw);
-                    return false;
-                };
-           // } catch (err) {
-           //     err.message = `unable to parse expression ${exp}: ${err.message}`;
-           //     throw err;
-           // }
+            var de = new DiceExpression(exp);
+            e.onclick = function (e) {
+                e.preventDefault();
+                var roll = de.roll();
+                alert('result: ' + roll.roll + " = " + roll.diceRaw);
+                return false;
+            };
+            // } catch (err) {
+            //     err.message = `unable to parse expression ${exp}: ${err.message}`;
+            //     throw err;
+            // }
         });
 }
 
-document.onreadystatechange = function(e) {
+document.onreadystatechange = function (e) {
     populateModifierElements();
     populateEvalForContent();
     registerRollExpressionHandlers();
 }
+
+let financialLedger = [
+    {
+        description: "Balance forward",
+        gp: 125
+    },
+    {
+        description: "Expenses in Sharn",
+        gp: -7
+    },
+    {
+        description: "Reward for retrieving medallion for MMM",
+        pp: 10
+    }
+];
+
+var app = new Vue({
+    el: '#characterSheet',
+    data: {
+        financialLedger: financialLedger
+    },
+    computed: {
+        financialSummary: function() {
+            console.log(this.financialLedger);
+            var aggregate = {
+                pp: 0,
+                gp: 0,
+                sp: 0
+            };
+            this.financialLedger.forEach(function(item) {
+                aggregate.pp = aggregate.pp + (item.pp || 0);
+                aggregate.gp = aggregate.gp + (item.gp || 0);
+            });
+            return aggregate;
+        }
+    }
+})
